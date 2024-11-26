76af61f933d76664c1964f432de3a603b7895e03
AdeleGropplero <adele.gdt@hotmail.com>
2024-11-26 16:41:26 +0100

    modifiche

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import TopBar from "./components/MyTopBar";
+import Footer from "./components/MyFooter";
+import Main from "./components/AllTheBooks";
 
 function App() {
   return (
     <>
       <TopBar />
+      {/* Creare l'alert */}
+      <Main />
+
+      <Footer />
     </>
   );
 }
